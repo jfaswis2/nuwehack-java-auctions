7d05d67875a65a19ab1296f24b1fdb3f40f19f04
Jhon <jfaswis2@gmail.com>
2024-03-30 23:51:35 +0100

    add objectives 1, 2, 3, 4

--- src/main/java/com/hackathon/hackathon/model/Art.java
+++ src/main/java/com/hackathon/hackathon/model/Art.java
@@ -6,11 +6,30 @@ package com.hackathon.hackathon.model;
  * - Se debe incluir la implementación del constructor public Art(String name, double initialPrice, Bidder currentBidder, String type, String artist, int yearCreated)
  * - Se deben incluir los métodos getter y setter de la clase Art
  */
-public class Art {
+
+public class Art extends Item{
     private String artist;
     private int yearCreated;
 
     public Art(String name, double initialPrice, Bidder currentBidder, String type, String artist, int yearCreated) {
-    
+        super(name, initialPrice, currentBidder, type);
+        this.artist = artist;
+        this.yearCreated = yearCreated;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public int getYearCreated() {
+        return yearCreated;
+    }
+
+    public void setYearCreated(int yearCreated) {
+        this.yearCreated = yearCreated;
     }
 }
